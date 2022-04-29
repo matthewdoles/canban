d2ad8f3289ad3d544dd91fba0c759c8e3a83e314
Matthew Doles <matteddied@gmail.com>
2022-04-28 23:24:05 -0500

    Route handling when not logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useAppSelector } from './hooks';
 import Board from './pages/Board';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 
 function App() {
+  const user = useAppSelector((state) => state.user.firebaseUser);
   return (
     <div className="min-h-screen min-w-screen bg-zinc-300" style={{ fontFamily: 'Roboto' }}>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/board/:id" element={<Board />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      {user === null ? (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/board/:id" element={<Board />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      )}
     </div>
   );
 }
